5aa2c5541ab60abdad9546f6b8928171f54a8310
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-04 14:32:54 +0200

    further work on routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const jwt = require("express-jwt");
+const { expressjwt: jwt } = require("express-jwt");
 
 const isAuthenticated = jwt({
   secret: process.env.JWT_SECRET,
