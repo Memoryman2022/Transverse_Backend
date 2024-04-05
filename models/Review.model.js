434d64b32946b9d765ad32b3b88edb8a245c735a
Memoryman2022 <rlacannon@gmail.com>
2024-04-05 14:00:30 +0200

    review post working

--- models/Review.model.js
+++ models/Review.model.js
@@ -4,9 +4,9 @@ const { Schema, model } = mongoose;
 const reviewSchema = new Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, //
   host: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, //
-  rating: { type: Number, required: true }, // quality of hosts
+  rating: { type: Number, required: true, min: 1, max: 5 }, // quality of hosts
   comments: { type: String, required: true }, //from guest about host
-  commentDate: { type: Date, default: Date.now }, //when the comment was created
+  //commentDate: { type: Date, default: Date.now }, //when the comment was created
 });
 
 module.exports = model("Review", reviewSchema);
--- routes/review.router.js
+++ routes/review.router.js
@@ -1,13 +1,25 @@
 const router = require("express").Router();
 const ReviewModel = require("../models/Review.model");
 const { AppError } = require("../middleware/error-handling");
-
+const UserModel = require("../models/User.model");
 //Post
 router.post("/create-a-review", async (req, res, next) => {
   try {
-    const review = await ReviewModel.create(req.body);
+    const { userName, hostName, rating, comments } = req.body;
+    const user = await UserModel.findOne({ name: userName });
+    const host = await UserModel.findOne({ name: hostName });
+    if (!user || !host) {
+      throw new AppError("User or Host not found", 404);
+    }
+    const review = await ReviewModel.create({
+      user: user._id,
+      host: host._id,
+      rating,
+      comments,
+    });
     res.status(201).json(review);
   } catch (error) {
+    console.error(error);
     next(new AppError("Failed to create review", 500));
   }
 });
@@ -43,7 +55,7 @@ router.put("/update-a-review/:reviewId", async (req, res, next) => {
   }
 });
 //Delete
-router.delete("/reviews/:id", async (req, res, next) => {
+router.delete("/review/:id", async (req, res, next) => {
   try {
     const deletedReview = await ReviewModel.findByIdAndDelete(req.params.id);
     if (!deletedReview) {
