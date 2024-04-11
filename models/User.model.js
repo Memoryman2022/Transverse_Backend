057c9097007863276e52c5c095bb52d087650e40
Memoryman2022 <rlacannon@gmail.com>
2024-04-11 13:20:26 +0200

    updated user model

--- models/User.model.js
+++ models/User.model.js
@@ -5,15 +5,15 @@ const userSchema = new Schema({
   userName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  // spokenLanguages: [
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     // required: true,
-  //     ref: "Language",
-  //   },
-  // ],
-  // hostedLanguages: [{ type: "String" }], // TODO: Also reference Language
-  // location: { type: "String" },
+  spokenLanguages: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Language",
+    },
+  ],
+  hostedLanguages: [{ type: "String" }], // TODO: Also reference Language
+  location: { type: "String" },
 });
 
 module.exports = model("User", userSchema);
